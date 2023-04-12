c27bd89b8aba15bc101eeb3a0d5b3bf3b89568f3
FairyyGenie <ch3801@nyu.edu>
2023-04-12 14:51:43 +0400

    receiver logic code

--- rdt_receiver.c
+++ rdt_receiver.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h> 
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -11,7 +11,6 @@
 
 #include "common.h"
 #include "packet.h"
-
 
 /*
  * You are required to change the implementation to support
@@ -22,98 +21,161 @@
 tcp_packet *recvpkt;
 tcp_packet *sndpkt;
 
-int main(int argc, char **argv) {
-    int sockfd; /* socket */
-    int portno; /* port to listen on */
-    int clientlen; /* byte size of client's address */
+int main(int argc, char **argv)
+{
+    int sockfd;                    /* socket */
+    int portno;                    /* port to listen on */
+    int clientlen;                 /* byte size of client's address */
     struct sockaddr_in serveraddr; /* server's addr */
     struct sockaddr_in clientaddr; /* client addr */
-    int optval; /* flag value for setsockopt */
+    int optval;                    /* flag value for setsockopt */
     FILE *fp;
     char buffer[MSS_SIZE];
     struct timeval tp;
 
-    /* 
-     * check command line arguments 
+    /*
+     * check command line arguments
      */
-    if (argc != 3) {
+    if (argc != 3)
+    {
         fprintf(stderr, "usage: %s <port> FILE_RECVD\n", argv[0]);
         exit(1);
     }
     portno = atoi(argv[1]);
 
-    fp  = fopen(argv[2], "w");
-    if (fp == NULL) {
+    fp = fopen(argv[2], "w");
+    if (fp == NULL)
+    {
         error(argv[2]);
     }
 
-    /* 
-     * socket: create the parent socket 
+    /*
+     * socket: create the parent socket
      */
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
 
-    /* setsockopt: Handy debugging trick that lets 
-     * us rerun the server immediately after we kill it; 
-     * otherwise we have to wait about 20 secs. 
-     * Eliminates "ERROR on binding: Address already in use" error. 
+    /* setsockopt: Handy debugging trick that lets
+     * us rerun the server immediately after we kill it;
+     * otherwise we have to wait about 20 secs.
+     * Eliminates "ERROR on binding: Address already in use" error.
      */
     optval = 1;
-    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, 
-            (const void *)&optval , sizeof(int));
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+               (const void *)&optval, sizeof(int));
 
     /*
      * build the server's Internet address
      */
-    bzero((char *) &serveraddr, sizeof(serveraddr));
+    bzero((char *)&serveraddr, sizeof(serveraddr));
     serveraddr.sin_family = AF_INET;
     serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
     serveraddr.sin_port = htons((unsigned short)portno);
 
-    /* 
-     * bind: associate the parent socket with a port 
+    // write structure to save out of order packet into buffer array
+    struct outorder
+    {
+        int seqnum;
+        tcp_packet *out;
+    };
+    struct outorder outoforder[1000];
+    // keep track of how many out of order packet there is
+    int howmany;
+
+    /*
+     * bind: associate the parent socket with a port
      */
-    if (bind(sockfd, (struct sockaddr *) &serveraddr, 
-                sizeof(serveraddr)) < 0) 
+    if (bind(sockfd, (struct sockaddr *)&serveraddr,
+             sizeof(serveraddr)) < 0)
         error("ERROR on binding");
 
-    /* 
+    /*
      * main loop: wait for a datagram, then echo it
      */
     VLOG(DEBUG, "epoch time, bytes received, sequence number");
 
     clientlen = sizeof(clientaddr);
-    while (1) {
+    while (1)
+    {
         /*
          * recvfrom: receive a UDP datagram from a client
          */
-        //VLOG(DEBUG, "waiting from server \n");
+        VLOG(DEBUG, "waiting from server \n");
         if (recvfrom(sockfd, buffer, MSS_SIZE, 0,
-                (struct sockaddr *) &clientaddr, (socklen_t *)&clientlen) < 0) {
+                     (struct sockaddr *)&clientaddr, (socklen_t *)&clientlen) < 0)
+        {
             error("ERROR in recvfrom");
         }
-        recvpkt = (tcp_packet *) buffer;
+        recvpkt = (tcp_packet *)buffer;
         assert(get_data_size(recvpkt) <= DATA_SIZE);
-        if ( recvpkt->hdr.data_size == 0) {
-            //VLOG(INFO, "End Of File has been reached");
+        if (recvpkt->hdr.data_size == 0)
+        {
+            VLOG(INFO, "End Of File has been reached");
             fclose(fp);
             break;
         }
-        /* 
-         * sendto: ACK back to the client 
+        /*
+         * sendto: ACK back to the client
          */
         gettimeofday(&tp, NULL);
         VLOG(DEBUG, "%lu, %d, %d", tp.tv_sec, recvpkt->hdr.data_size, recvpkt->hdr.seqno);
+        /*if condition to screen whether it is out of order packet and if so
+        we need to buffer the packet
+        the buffer needs to be a priority queue and
+        so when we write it back to the file it is in order*/
+        for (int i = 0; i < howmany; i++)
+        {
+            if ((&outoforder[i])->seqnum!=NULL && (&outoforder[i])->seqnum == recvpkt->hdr.seqno + 1)
+            {
+                 // write into file only when receving correct sequence packet
+                    fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
+                    fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
+                    fseek(fp, (&outoforder[i])->seqnum, SEEK_SET);
+                    fwrite((&outoforder[i])->out->data, 1, (&outoforder[i])->out->hdr.data_size, fp);
+                //cumulative ACK
+                    sndpkt = make_packet(0);
+                    sndpkt->hdr.ackno = (&outoforder[i])->out->hdr.seqno + (&outoforder[i])->out->hdr.data_size;
+                    sndpkt->hdr.ctr_flags = ACK;
+                    if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
+                               (struct sockaddr *)&clientaddr, clientlen) < 0)
+                    {
+                        error("ERROR in sendto");
+                    }
+                    /*send the packet of ACK the original ACk sequence number */
+            }
+            else
+            {
+                if (recvpkt->hdr.seqno != sndpkt->hdr.ackno)
+                {
+                    printf("out of order packet number %d into buffer", recvpkt->hdr.seqno);
+                    outoforder[howmany].seqnum = recvpkt->hdr.seqno;
+                    outoforder[howmany].out = recvpkt;
+                    howmany++;
+                    // send the old acknowledgement info
+                    if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
+                               (struct sockaddr *)&clientaddr, clientlen) < 0)
+                    {
+                        error("ERROR in sendto");
+                    }
+                }
+                else
+                {
+                    // write into file only when receving correct sequence packet
+                    fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
+                    fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
 
-        fseek(fp, recvpkt->hdr.seqno, SEEK_SET);
-        fwrite(recvpkt->data, 1, recvpkt->hdr.data_size, fp);
-        sndpkt = make_packet(0);
-        sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
-        sndpkt->hdr.ctr_flags = ACK;
-        if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0, 
-                (struct sockaddr *) &clientaddr, clientlen) < 0) {
-            error("ERROR in sendto");
+                    sndpkt = make_packet(0);
+                    sndpkt->hdr.ackno = recvpkt->hdr.seqno + recvpkt->hdr.data_size;
+                    sndpkt->hdr.ctr_flags = ACK;
+                    if (sendto(sockfd, sndpkt, TCP_HDR_SIZE, 0,
+                               (struct sockaddr *)&clientaddr, clientlen) < 0)
+                    {
+                        error("ERROR in sendto");
+                    }
+                    /*send the packet of ACK the original ACk sequence number */
+                }
+            }
         }
     }
 
