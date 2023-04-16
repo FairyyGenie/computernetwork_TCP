1c43fe3f567d14c5b4b797a995d74d30012facfa
Kenneth Ding <kending19@gmail.com>
2023-04-16 16:20:52 +0400

    bingo

--- rdt_sender.c
+++ rdt_sender.c
@@ -189,7 +189,7 @@ int main (int argc, char **argv)
             }
 			bytes[packetBase+window_size] = bytes[packetBase+window_size-1] + length;
 			sndpkt = make_packet(length);
-            sndpkt->hdr.seqno = bytes[packetBase];
+            sndpkt->hdr.seqno = bytes[packetBase+window_size];
             memcpy(sndpkt->data, buffer, length);
 			if(sendto(sockfd, sndpkt, TCP_HDR_SIZE + get_data_size(sndpkt), 0, ( const struct sockaddr *)&serveraddr, serverlen) < 0){
             	error("sendto");
