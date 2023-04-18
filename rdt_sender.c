8eb07fd3378f9efb93484b9d787a2e823ff50f0d
FairyyGenie <ch3801@nyu.edu>
2023-04-19 02:38:07 +0400

    .

--- rdt_sender.c
+++ rdt_sender.c
@@ -176,7 +176,6 @@ int main(int argc, char **argv)
                 sndpkt = make_packet(length);
                 sndpkt->hdr.seqno = bytes[i];
                 memcpy(sndpkt->data, buffer, length);
-                printf("%s", sndpkt->data);
                 if (sendto(sockfd, sndpkt, TCP_HDR_SIZE + get_data_size(sndpkt), 0, (const struct sockaddr *)&serveraddr, serverlen) < 0)
                 {
                     error("sendto");
